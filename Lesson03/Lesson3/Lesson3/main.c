1118c38f634dab706e53405d803a12fc4b2a8f6c
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2013-10-08 13:58:36 +0200

    Initial Commit

---
+++ Lesson03/Lesson3/Lesson3/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Lesson3
+//
+//  Created by Pavel Jurásek on 08.10.13.
+//  Copyright (c) 2013 Pavel Jurásek. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
