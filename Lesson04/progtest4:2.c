9938c87b1070769be24d29b617e3c505bdbf058b
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2013-10-11 11:13:26 +0200

    Test4.2: Refactor code

--- Lesson04/progtest4:2.c
+++ Lesson04/progtest4:2.c
@@ -1,53 +1,42 @@
-/*
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 
 #include <stdio.h>
-#include <stdlib.h>
 
-
-//counts the fibonacci sequence and returns the n'th number
-int fibs(int n) {
-    int m = 1;
-    int k = 0;
-    int i;
+int fibonacci(int n)
+{
+    int last_prev = 0, last = 1, i;
+    int result = 0;
     
     for (i = 1; i <= n; i++) {
-        int tmp = m + k;
-        m = k;
-        k = tmp;
+        result = last_prev + last;
+        last_prev = last;
+        last = result;
     }
-    return m;
+    
+    return result;
 }
 
-int main(void){
-    int i = 2; //start with number 2, because of Progtest ...
+int main()
+{
+    int i = 3;
     char c = 0;
+    
     printf("The Fibonacci sequence starts with 1,1,2\n");
+    
     while(c != 'n') {
-        i++;
-        if(i > 4){
-            printf("Display next? [any key/n]\n");
-            c = getchar(); //user input
-            if(c != '\n'){ //newline character?
-                getchar(); //newline character!
-            }
-            if(c == 'n'){return 0;} // n - exit
-            printf("Next number in Fibonacci sequence is: %d\n",fibs(i));
+
+        printf("Display next? [any key/n]\n");
+
+        if ((c = getchar()) != '\n') {
+            getchar(); // catch newline character
         }
+    
+        if (c == 'n') {
+            return 0;
+        }
+            
+        printf("Next number in Fibonacci sequence is: %d\n", fibonacci(i++));
+
     }
+    
     return 0;
 }
-
